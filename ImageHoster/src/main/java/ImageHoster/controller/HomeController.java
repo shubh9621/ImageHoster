7cd56475c2a4c64a11216e2f000d8ed16ce3ca3d
shubh9621 <42244464+shubh9621@users.noreply.github.com>
2020-03-16 02:29:17 +0530

    Add files via upload

---
+++ ImageHoster/src/main/java/ImageHoster/controller/HomeController.java
@@ -0,0 +1,30 @@
+package ImageHoster.controller;
+
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    public HomeController() {
+        System.out.println("*** HomeController ***");
+    }
+    @Autowired
+    private ImageService imageService;
+
+    @RequestMapping("/")
+    public String getAllImages(Model model) {
+        List<Image> images = imageService.getAllImages();
+        model.addAttribute("images", images);
+        return "index";
+    }
+}
---
+++ ImageHoster/src/main/java/ImageHoster/service/CommentService.java
@@ -0,0 +1,23 @@
+package ImageHoster.service;
+
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import ImageHoster.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    /**
+     * @param comment
+     */
+    public void addComment(Comment comment) {
+        comment.setCreatedDate(LocalDate.now());
+        commentRepository.addComment(comment);
+    }
+}
